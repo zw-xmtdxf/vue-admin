9ac926f7d7dd2be4fb06ada5346671c5180af624
zw-xmtdxf <64604954+zw-xmtdxf@users.noreply.github.com>
2020-07-23 17:13:13 +0800

    创建VUE

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import Home from "../views/Home.vue";
+// import Login from "../views/Login/index.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "login"
+  },
+  {
+    path: "/Login",
+    name: "Login",
+    component: () => import("../views/Login/index.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ src/views/Login/index.vue
@@ -0,0 +1,19 @@
+// 每个.vue文件都有三块东西，template/模板，script/JS，style/样式，模板外层一定要有根元素包着
+<template>
+    <div id="login">这里登陆页面
+       <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+    </div>
+</template>
+<script></script>
+<style lang="scss" scoped>
+#login{
+    background-color: #344a5f;
+    height: 100vh;
+
+}
+</style>
